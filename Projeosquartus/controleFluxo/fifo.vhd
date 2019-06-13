4d49d3ae4212bf1ac04ba26f0f12fc939ebc3e8b
Deco Rodrigues <falecomandresantos@gmail.com>
2019-06-13 10:28:10 -0300

    Teste

--- Projeosquartus/controleFluxo/fifo.vhd
+++ Projeosquartus/controleFluxo/fifo.vhd
@@ -4,7 +4,7 @@
 -- MODULE: scfifo 
 
 -- ============================================================
--- File Name: fifo.vhd
+-- File Name: Fifo.vhd
 -- Megafunction Name(s):
 -- 			scfifo
 --
@@ -39,7 +39,7 @@ USE ieee.std_logic_1164.all;
 LIBRARY altera_mf;
 USE altera_mf.all;
 
-ENTITY fifo IS
+ENTITY Fifo IS
 	PORT
 	(
 		clock		: IN STD_LOGIC ;
@@ -49,17 +49,19 @@ ENTITY fifo IS
 		empty		: OUT STD_LOGIC ;
 		full		: OUT STD_LOGIC ;
 		q		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		usedw		: OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
+		usedw		: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
 	);
-END fifo;
+END Fifo;
 
 
 ARCHITECTURE SYN OF fifo IS
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (8 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (7 DOWNTO 0);
 	SIGNAL sub_wire1	: STD_LOGIC ;
 	SIGNAL sub_wire2	: STD_LOGIC ;
 	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (7 DOWNTO 0);
+
+
 
 	COMPONENT scfifo
 	GENERIC (
@@ -78,7 +80,7 @@ ARCHITECTURE SYN OF fifo IS
 			clock	: IN STD_LOGIC ;
 			data	: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 			rdreq	: IN STD_LOGIC ;
-			usedw	: OUT STD_LOGIC_VECTOR (8 DOWNTO 0);
+			usedw	: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
 			empty	: OUT STD_LOGIC ;
 			full	: OUT STD_LOGIC ;
 			q	: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
@@ -87,7 +89,7 @@ ARCHITECTURE SYN OF fifo IS
 	END COMPONENT;
 
 BEGIN
-	usedw    <= sub_wire0(8 DOWNTO 0);
+	usedw    <= sub_wire0(7 DOWNTO 0);
 	empty    <= sub_wire1;
 	full    <= sub_wire2;
 	q    <= sub_wire3(7 DOWNTO 0);
@@ -96,11 +98,11 @@ BEGIN
 	GENERIC MAP (
 		add_ram_output_register => "OFF",
 		intended_device_family => "Cyclone II",
-		lpm_numwords => 512,
+		lpm_numwords => 516,
 		lpm_showahead => "OFF",
 		lpm_type => "scfifo",
 		lpm_width => 8,
-		lpm_widthu => 9,
+		lpm_widthu => 8,
 		overflow_checking => "ON",
 		underflow_checking => "ON",
 		use_eab => "ON"
@@ -129,7 +131,7 @@ END SYN;
 -- Retrieval info: PRIVATE: AlmostFullThr NUMERIC "-1"
 -- Retrieval info: PRIVATE: CLOCKS_ARE_SYNCHRONIZED NUMERIC "0"
 -- Retrieval info: PRIVATE: Clock NUMERIC "0"
--- Retrieval info: PRIVATE: Depth NUMERIC "512"
+-- Retrieval info: PRIVATE: Depth NUMERIC "256"
 -- Retrieval info: PRIVATE: Empty NUMERIC "1"
 -- Retrieval info: PRIVATE: Full NUMERIC "1"
 -- Retrieval info: PRIVATE: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
@@ -158,11 +160,11 @@ END SYN;
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: ADD_RAM_OUTPUT_REGISTER STRING "OFF"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone II"
--- Retrieval info: CONSTANT: LPM_NUMWORDS NUMERIC "512"
+-- Retrieval info: CONSTANT: LPM_NUMWORDS NUMERIC "256"
 -- Retrieval info: CONSTANT: LPM_SHOWAHEAD STRING "OFF"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "scfifo"
 -- Retrieval info: CONSTANT: LPM_WIDTH NUMERIC "8"
--- Retrieval info: CONSTANT: LPM_WIDTHU NUMERIC "9"
+-- Retrieval info: CONSTANT: LPM_WIDTHU NUMERIC "8"
 -- Retrieval info: CONSTANT: OVERFLOW_CHECKING STRING "ON"
 -- Retrieval info: CONSTANT: UNDERFLOW_CHECKING STRING "ON"
 -- Retrieval info: CONSTANT: USE_EAB STRING "ON"
@@ -172,7 +174,7 @@ END SYN;
 -- Retrieval info: USED_PORT: full 0 0 0 0 OUTPUT NODEFVAL "full"
 -- Retrieval info: USED_PORT: q 0 0 8 0 OUTPUT NODEFVAL "q[7..0]"
 -- Retrieval info: USED_PORT: rdreq 0 0 0 0 INPUT NODEFVAL "rdreq"
--- Retrieval info: USED_PORT: usedw 0 0 9 0 OUTPUT NODEFVAL "usedw[8..0]"
+-- Retrieval info: USED_PORT: usedw 0 0 8 0 OUTPUT NODEFVAL "usedw[7..0]"
 -- Retrieval info: USED_PORT: wrreq 0 0 0 0 INPUT NODEFVAL "wrreq"
 -- Retrieval info: CONNECT: @clock 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: @data 0 0 8 0 data 0 0 8 0
@@ -181,10 +183,10 @@ END SYN;
 -- Retrieval info: CONNECT: empty 0 0 0 0 @empty 0 0 0 0
 -- Retrieval info: CONNECT: full 0 0 0 0 @full 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 8 0 @q 0 0 8 0
--- Retrieval info: CONNECT: usedw 0 0 9 0 @usedw 0 0 9 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL fifo.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL fifo.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL fifo.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL fifo.bsf FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL fifo_inst.vhd FALSE
+-- Retrieval info: CONNECT: usedw 0 0 8 0 @usedw 0 0 8 0
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Fifo.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Fifo.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Fifo.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Fifo.bsf FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL Fifo_inst.vhd FALSE
 -- Retrieval info: LIB_FILE: altera_mf
